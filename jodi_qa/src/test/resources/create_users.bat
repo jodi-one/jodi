ba0d1c9105d35cb1380703dc978729c2c40756fc
Duke Jodi <54733921+jodi-one@users.noreply.github.com>
2021-04-05 17:13:55 +0200

    create schemas on vbox or oci

---
+++ jodi_qa/src/test/resources/create_users.bat
@@ -0,0 +1,86 @@
+set URL=""
+set LB_DB_PWD=""
+set ORCL_SYS_USER_PWD=""
+set ORCL_SYS_USER=""
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN1 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN2 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN3 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN4 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN5 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN6 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN7 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN8 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN9 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN10 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN11 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN12 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN13 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN14 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN15 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN16 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN17 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN18 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN19 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN20 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN21 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN22 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN23 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN24 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN25 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN26 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN27 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN28 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN29 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EXN30 %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG1 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG2 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG3 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG4 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG5 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG6 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG7 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG8 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG9 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG10 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG11 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG12 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG13 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG14 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG15 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG16 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG17 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG18 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG19 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG20 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG21 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG22 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG23 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG24 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG25 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG26 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG27 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG28 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG29 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql STG30 %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql ODITMP %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql MTM %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql MCM %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql ERM %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql INF %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql MCV %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql DWHPUBLIC %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql EDW %LB_DB_PWD% DATA TEMP
+
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql SBX1 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql SBX2 %LB_DB_PWD% DATA TEMP
+sql.exe %ORCL_SYS_USER%/%ORCL_SYS_USER_PWD%@%URL% as sysdba @create_user.sql SBX3 %LB_DB_PWD% DATA TEMP
